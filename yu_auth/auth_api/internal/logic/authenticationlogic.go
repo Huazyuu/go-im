cada3185fc8428a622bac5311c879d9d18cc9e62
zyuu <zyuuforyu@gmail.com>
2025-03-26 17:03:46 +0800

    feat(auth_api): 认证服务 auth_api 用户认证接口初步完成

--- yu_auth/auth_api/internal/logic/authenticationlogic.go
+++ yu_auth/auth_api/internal/logic/authenticationlogic.go
@@ -2,6 +2,10 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"github.com/go-redis/redis"
+	"server/utils/jwt"
 
 	"github.com/zeromicro/go-zero/core/logx"
 	"server/yu_auth/auth_api/internal/svc"
@@ -21,8 +25,22 @@ func NewAuthenticationLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Au
 	}
 }
 
-func (l *AuthenticationLogic) Authentication() (resp string, err error) {
-	// todo: add your logic here and delete this line
+func (l *AuthenticationLogic) Authentication(token string) (resp string, err error) {
+	if token == "" {
+		return "", errors.New("请传入token")
+	}
 
-	return
+	_, err = jwt.ParseToken(token, l.svcCtx.Config.Auth.AccessSecret)
+	if err != nil {
+		return "", errors.New("认证失败")
+	}
+
+	// 是否在注销redis
+	key := fmt.Sprintf("logout_%s", token)
+	_, err = l.svcCtx.Redis.Get(key).Result()
+	if !errors.Is(err, redis.Nil) {
+		return "", errors.New("认证失败,认证过期")
+	}
+	resp = "认证通过"
+	return resp, nil
 }
